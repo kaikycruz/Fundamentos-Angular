2195f307e085c10932581aa47397acbbd122a809
kaikycruz <kaikycruz20@gmail.com>
2024-06-03 23:31:57 -0300

    modularizacao

--- projeto-inicial/src/app/app.module.ts
+++ projeto-inicial/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
-import { CardRoxoComponent } from './card-roxo/card-roxo.component';
+import { CardsModule } from './cards/cards.module';
+
 
 @NgModule({
-  declarations: [AppComponent, CardComponent, CardRoxoComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CardsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
